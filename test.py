888dba0830c8efddf340ea9b17d683394acd5c60
Jihwa <wlghk1654@gmail.com>
2021-11-23 16:37:56 +0900

    footprint error

--- test.py
+++ test.py
@@ -18,75 +18,69 @@ class first(Tk):
 		self.resizable(False, False)
 		#footprint routine
 		global i
-		i = 14
+		i = 21
 
 	def BackGroundSetting(self, bgname):
 		self.backGroundImage=PhotoImage(file=pwd+bgname+".png")
 		self.backGroundImageLabel=Label(self, image=self.backGroundImage)
 		self.backGroundImageLabel.place(x=0, y=0)
 
-	def tem_but(self):
-		src=Image.open(pwd+"NextButton.png")
-		src=src.resize((100, 100), Image.ANTIALIAS)
+	def Button1(self):
+		src=Image.open(pwd+"button1.png")
+		src=src.resize((350, 150), Image.ANTIALIAS)
 		self.nextButtonImage=ImageTk.PhotoImage(src)
-		self.nextButton=Button(self, image=self.nextButtonImage, command=self.tem_but_clk, border=0, cursor='heart')
-		self.nextButton.place(x=450, y=550)
+		self.nextButton=Button(self, image=self.nextButtonImage, command=self.Button1_clk, border=0, cursor='heart')
+		self.nextButton.place(x=450, y=480)
 
-	def tem_but_clk(self):
-		First.BackGroundSetting("background1")
+	def Button1_clk(self):
+		First.BackGroundSetting("background2")
 		print('page2')
 		First.Button2_Y()
 		First.Button2_N()
 	
 	def Button2_Y(self):
-		src=Image.open(pwd+"NextButton.png")
-		src=src.resize((100, 100), Image.ANTIALIAS)
+		src=Image.open(pwd+"button2_Y.png")
+		src=src.resize((200, 100), Image.ANTIALIAS)
 		self.yesButtonImage=ImageTk.PhotoImage(src)
 		self.lab_Button_Y=Button(self, image=self.yesButtonImage, command=self.Button2_Y_clk, border=0, cursor='heart')
-		self.lab_Button_Y.place(x=450, y=550)
+		self.lab_Button_Y.place(x=700, y=450)
 
 	def Button2_Y_clk(self):
 		print('page3')
-		First.BackGroundSetting("background1")
-		First.Button3()
+		First.BackGroundSetting("background3")
+		First.tracking()
 
 	def Button2_N(self):
-		src=Image.open(pwd+"NextButton.png")
-		src=src.resize((100, 100), Image.ANTIALIAS)
+		src=Image.open(pwd+"button2_N.png")
+		src=src.resize((200, 100), Image.ANTIALIAS)
 		self.noButtonImage=ImageTk.PhotoImage(src)
 		self.lab_Button_N=Button(self, image=self.noButtonImage, command=self.Button2_N_clk, border=0)
-		self.lab_Button_N.place(x=750, y=550)
+		self.lab_Button_N.place(x=950, y=450)
 
 	def Button2_N_clk(self):
 		print('page1')
 		First.BackGroundSetting("background1")
-		First.tem_but()
-
-	def Button3(self):
-		src=Image.open(pwd+"NextButton.png")
-		src=src.resize((200, 200), Image.ANTIALIAS)
-		self.nextButtonImage=ImageTk.PhotoImage(src)
-		self.nextButton=Button(self, image=self.nextButtonImage, command=self.Button3_clk, border=0, cursor='heart')
-		self.nextButton.place(x=950, y=500)
-
-	def Button3_clk(self):
-		First.tracking()
+		First.Button1()
 
 	def tracking(self):
 		print('tracking')
-		#footprint background
-		First.BackGroundSetting("background_gray")
 		#footprint image
-		src=Image.open(pwd+"footprint_noback.png")
-		src=src.resize((30, 30), Image.ANTIALIAS)
-		self.footprintImage=ImageTk.PhotoImage(src)
-		self.lab_foot_L=Label(image =self.footprintImage, border = 0)
-		self.lab_foot_R=Label(image =self.footprintImage, border = 0)
-		#cat image
-		src=Image.open(pwd+"cat_back.png")
-		src=src.resize((100, 100), Image.ANTIALIAS)
-		self.catImage=ImageTk.PhotoImage(src)
-		self.lab_catImage=Label(image =self.catImage, border = 0)
+		src=Image.open(pwd+"footprint_0.png")
+		src=src.resize((50, 50), Image.ANTIALIAS)
+		self.footprintImage0=ImageTk.PhotoImage(src)
+		src=Image.open(pwd+"footprint_50.png")
+		src=src.resize((50, 50), Image.ANTIALIAS)
+		self.footprintImage50=ImageTk.PhotoImage(src)
+		src=Image.open(pwd+"footprint_80.png")
+		src=src.resize((50, 50), Image.ANTIALIAS)
+		self.footprintImage80=ImageTk.PhotoImage(src)
+		self.lab_foot_L0=Label(image =self.footprintImage0, border = 0)
+		self.lab_foot_R1=Label(image =self.footprintImage0, border = 0)
+		self.lab_foot_L2=Label(image =self.footprintImage50, border = 0)
+		self.lab_foot_R3=Label(image =self.footprintImage50, border = 0)
+		self.lab_foot_L4=Label(image =self.footprintImage80, border = 0)
+		self.lab_foot_R5=Label(image =self.footprintImage80, border = 0)
+
 
 		global fp_finish
 		fp_finish =0
@@ -95,28 +89,34 @@ class first(Tk):
 	def footprint_choose(self):
 		global i 
 		global fp_finish
-		if i == -1:
-			i = 13
+		if i == 1:
+			i = 21
 		if i % 2 == 0:
 			i -= 1
-			First.footprint_L()
+			First.footprint_L(self.lab_foot_L0)
 		else:
 			i -= 1
-			First.footprint_R()
+			First.footprint_R(self.lab_foot_R1)
+
 		if fp_finish == 0:
 			threading.Timer(1, First.footprint_choose).start()
 
-	def footprint_L(self):
+	def footprint_L(self, foot):
 		global i
 		y_foot_L = i*30
-		self.lab_catImage.place(x=650, y=y_foot_L-100)
-		self.lab_foot_L.place(x=660, y=y_foot_L)
-		
-	def footprint_R(self):
+		foot.place(x=955, y=y_foot_L)
+		if y_foot_L <570:
+			self.lab_foot_L2.place(x=955, y=y_foot_L+60)
+		if y_foot_L <510:
+			self.lab_foot_L4.place(x=955, y=y_foot_L+120)
+	def footprint_R(self, foot):
 		global i
 		y_foot_R = i*30 
-		self.lab_catImage.place(x=650, y=y_foot_R-100)
-		self.lab_foot_R.place(x=700, y=y_foot_R)
+		foot.place(x=1000, y=y_foot_R)
+		if y_foot_R <570:
+			self.lab_foot_R3.place(x=1000, y=y_foot_R+60)
+		if y_foot_R <510:
+			self.lab_foot_R5.place(x=1000, y=y_foot_R+120)
 	
 	def Button5(self):
 		src=Image.open(pwd+"NextButton.png")
@@ -148,5 +148,5 @@ class first(Tk):
 if __name__ == "__main__":
 	First = first()
 	First.BackGroundSetting("background1")
-	First.tem_but()
+	First.Button1()
 	First.mainloop()
